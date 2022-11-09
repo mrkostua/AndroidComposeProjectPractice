3533a00bdddcb550dc809020125e9bc597d25786
prysiazhnyik <prysiazhnyik@mydigicode.com>
2022-11-09 13:23:02 +0200

    Layouts and Material Design

--- app/src/main/java/com/mrkostua/androidcomposeproject/MainActivity.kt
+++ app/src/main/java/com/mrkostua/androidcomposeproject/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.mrkostua.androidcomposeproject
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mrkostua.androidcomposeproject.ui.theme.AndroidComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,15 +36,57 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(45.dp)
+                .padding(5.dp)
+                .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.button
+                )
+            }
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+    }
+
+}
+
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
     AndroidComposeProjectTheme {
-        Greeting("Android")
+        Surface {
+            MessageCard(msg = Message("Kostia", "Hello"))
+        }
     }
 }
